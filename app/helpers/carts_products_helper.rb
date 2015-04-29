c2dcffc9d4ee7279ce4e35523b0f99b52e0f6745
ripan <er.ripankumar@gmail.com>
2015-04-29 11:23:26 +0100

    Fix price issue

--- app/helpers/carts_products_helper.rb
+++ app/helpers/carts_products_helper.rb
@@ -1,10 +1,9 @@
 module CartsProductsHelper
   def get_discounted_product_price(quantity,product)
+  	price = product.price
     if quantity > 1
       pr = PromotionalRule.find_by_product_id(product.id)
-      price = pr.discount_price
-    else
-      price = product.price
+      price = pr.discount_price if pr
     end
     return price
   end
